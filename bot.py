5eac3fc4b369d7fbaf266229f012ff7ad52cc691
= <christopher.jones2559@gmail.com>
2025-09-03 20:03:53 +0100

    initial

---
+++ bot.py
@@ -0,0 +1,79 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    channel = bot.get_channel(1412831975147962472)
+    await channel.send("Hey gamers!")
+
+@bot.event
+async def on_close():
+    print("Bot is shutting down")
+    channel = bot.get_channel(1412831975147962472)
+    await channel.send("I'm off!")
+
+@bot.event
+async def on_disconnect():
+    print("Bot has disconnected")
+    channel = bot.get_channel(1412831975147962472)
+    await channel.send("I have disconnected")
+
+@bot.event
+async def on_resumed():
+    print("Bot has resumed")
+    channel = bot.get_channel(1412831975147962472)
+    await channel.send("I have resumed")
+
+@bot.command(help="Echos back the message after the command")
+async def echo(ctx, *, arg):
+    print(f"Echo triggered.")
+    await ctx.send(arg)
+
+@bot.command(help="Echos back the message after the command in upper case")
+async def upper(ctx, *, arg):
+    print(f"Upper triggered.")
+    await ctx.send(arg.upper())
+
+@bot.command(help="Echos back the message after the command in lower case")
+async def lower(ctx, *, arg):
+    print(f"Lower triggered.")
+    await ctx.send(arg.lower())
+
+@bot.command(help="Echos back the message after the command in title case")
+async def title(ctx, *, arg):
+    print(f"Title triggered.")
+    newMessage = ""
+    for word in arg.split():
+        word = word[0].upper() + word[1:].lower()
+        newMessage += word + " "
+    
+    await ctx.send(newMessage)
+
+@bot.command(help="Sends a link to my cool site")
+async def site(ctx):
+    print(f"Site triggered.")
+    await ctx.send("Doesn't exist yet sorry but it might one day!")
+
+bot.remove_command("help")  # remove the default help so it can be replaced
+@bot.command(help="Shows this message")
+async def help(ctx):
+    print(f"Help triggered.")
+    embed = discord.Embed(title="Help", description="Here are the commands", color=discord.Color.blurple())
+    for cmd in bot.commands:
+        embed.add_field(name=f"!{cmd.name}", value=cmd.help or "No description", inline=False)
+    await ctx.send(embed=embed)
+
+
+
+# Load token and run
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot.run(TOKEN)
